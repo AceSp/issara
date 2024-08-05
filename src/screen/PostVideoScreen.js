96d4181378c60f82c98f47269b463e5b0eaa56f2
AceSp <56382861+AceSp@users.noreply.github.com>
2024-08-05 13:40:08 +0700

    feat: add PostVideoScreen component

---
+++ src/screen/PostVideoScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PostVideoScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Post Video Screen</Text>
+    </View>
+  );
+};
+
+export default PostVideoScreen;
