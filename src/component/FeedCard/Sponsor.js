4896c1b70d152c70cb6eb39a073fddfa1b90fdbf
AceSp <56382861+AceSp@users.noreply.github.com>
2024-09-14 16:31:50 +0700

    refactor: simplify Sponsor component and remove unused styles

--- src/component/FeedCard/Sponsor.js
+++ src/component/FeedCard/Sponsor.js
@@ -21,10 +21,6 @@ import {
 moment.locale('th');
 
 function Sponsor(props) {
-    const sponsor = props.shop? props.shop 
-                    : props.product ? props.product
-                    : null;
-
     // if(props.isAdmob) return (
     //   <NativeAdView 
     //     style={{
@@ -93,137 +89,11 @@ function Sponsor(props) {
     //   </NativeAdView>
     // )
     return (
-        <TouchableRipple 
-            onPress={() => props.navigation? 
-                props.navigation.navigate('Shop', { id: sponsor? sponsor.id : null })
-                :
-                null
-            } 
-            style={{ width: '100%' }}
-            >
-            <View>
-                <Card.Title
-                    style={styles.root}
-                    title="title"
-                    // title={sponsor? sponsor.name : null}
-                    // subtitle={props.text}
-                    subtitle="subtitle"
-                    subtitleStyle={materialTall.body1}
-                    left={(p) => <Image 
-                        style={styles.image} 
-                        source={sponsor
-                            ? props.image 
-                            : require('../../assets/pic/rose-blue-flower-rose-blooms-67636.jpeg')} />}
-                    leftStyle={styles.leftStyle}
-                    rightStyle={styles.rightStyle}
-                    />    
-                <View style={styles.bottomView}>
-                    <View style={styles.sponsorTag}>
-                        <Text style={[
-                            iOSUIKitTall.caption2,
-                            { color: iOSColors.red }
-                            ]}>
-                            ผู้สนับสนุนคอลัมน์
-                        </Text>
-                    </View>
-                    <Text style={[iOSUIKitTall.caption2, styles.sponsorName]}>
-                        {sponsor.itemName}hello
-                    </Text>
-                </View>
-            </View>
-        </TouchableRipple>
-        
+        <View></View>
     )
 }
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: "flex-start", 
-        paddingLeft: 10,
-        paddingBottom: 15,
-        backgroundColor: 'white'
-    },
-    image: {
-        height: 80,
-        width: 120,
-        marginRight: 5,
-        borderRadius: 5,
-        overflow: 'hidden'
-    },
-    sponsorTag: {
-        borderRadius: 5,
-        marginLeft: 10,
-        marginTop: 2,
-        paddingHorizontal: 10,
-        paddingVertical: 1,
-        borderColor: iOSColors.red,
-        borderWidth: 1
-    },
-    leftStyle: {
-        marginRight: 85, 
-        marginTop: 25, 
-        marginBottom: 10
-    },
-    rightStyle: {
-        position: 'absolute', 
-        top: 58, 
-        left: 115
-    },
-    sponsorView: {
-        marginLeft: 20,
-         flexDirection: 'row'
-    },
-    bottomView: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    sponsorName: {
-        marginLeft: 25,
-        color: iOSColors.gray
-    }
 })
 
 export default memo(Sponsor);
-
-/*
-    return (
-        <Card.Title
-          style={{ marginVertical: 2, alignItems: "flex-start", paddingLeft: 10 }}
-          rightStyle={{ height: 40 }}
-          titleStyle={{}}
-          title="adasfasgfasfasfasfasf"
-          subtitle={props.text}
-          subtitleStyle={materialTall.body1}
-          left={(p) => <Image style={styles.image} source={require('../../assets/pic/rose-blue-flower-rose-blooms-67636.jpeg')} />}
-          leftStyle={{marginRight: 85, marginTop: 20}}
-          rightStyle={{position: 'absolute', top: 60, right: 5}}
-          right={() => {
-              return(
-                    <View style={styles.followedButton}>
-                        <Text style={styles.followText}>ผู้สนับสนุนคอลัมน์</Text>
-                    </View>
-              )
-          } }
-        />    
-    )
-}
-
-const styles = StyleSheet.create({
-    image: {
-        height: 90,
-        width: 120,
-        marginRight: 5,
-        borderRadius: 5,
-        overflow: 'hidden'
-    },
-    followedButton: {
-        borderRadius: 5,
-        marginLeft: 20,
-        marginTop: 2,
-        paddingHorizontal: 10,
-        paddingVertical: 1,
-        backgroundColor: iOSColors.red
-    },
-})
-
-*/
