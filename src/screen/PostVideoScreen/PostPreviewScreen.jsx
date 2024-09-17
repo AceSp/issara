d1f9345db50b15d78287694e4e815a53a2a47ecf
AceSp <56382861+AceSp@users.noreply.github.com>
2024-09-17 13:28:37 +0700

    feat: add video trimming and background upload functionality to PostPreviewScreen

--- src/screen/PostVideoScreen/PostPreviewScreen.jsx
+++ src/screen/PostVideoScreen/PostPreviewScreen.jsx
@@ -1,13 +1,17 @@
 import React, { 
   useState, 
 } from 'react';
-import React from 'react';
 import { 
   View, 
   Text, 
   StyleSheet, 
   TouchableOpacity 
 } from 'react-native';
+import { 
+  closeEditor, 
+  showEditor 
+} from 'react-native-video-trim';
+
 import { VideoPlayer } from '../../component/Video/views';
 import PostTextModal from '../../component/PostTextModal';
 import IonIcon from 'react-native-vector-icons/Ionicons';
@@ -15,16 +19,86 @@ import IonIcon from 'react-native-vector-icons/Ionicons';
 function PostPreviewScreen({
   route
 }) {
-  const { 
-    uri
-  } = route.params;
+  // const { 
+  //   uri
+  // } = route.params;
   const [paused, setPaused] = useState(false);
   const [isModalVisible, setModalVisible] = useState(false)
   const toggleVideo = () => {
     setPaused(!paused);
   }
-  console.log("-----------PostPreviewScreen")
-  console.log(uri)
+
+  const handleUpload = async (filePath) => {
+    const options = {
+      taskName: 'FileUpload',
+      taskTitle: 'Uploading File',
+      taskDesc: 'Progress',
+      taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+      },
+      color: '#ff00ff',
+      //change this for opening the app from notification
+      linkingURI: 'uploadFile',
+    };
+    await BackgroundService.start(() => uploadFileInChunks(filePath, me.id), options);
+  };
+
+  useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.VideoTrim);
+    const subscription = eventEmitter.addListener('VideoTrim', (event) => {
+      switch (event.name) {
+        case 'onLoad': {
+          // on media loaded successfully
+          console.log('onLoadListener', event);
+          break;
+        }
+        case 'onShow': {
+          console.log('onShowListener', event);
+          break;
+        }
+        case 'onHide': {
+          console.log('onHide', event);
+          break;
+        }
+        case 'onStartTrimming': {
+          console.log('onStartTrimming', event);
+          break;
+        }
+        case 'onFinishTrimming': {
+          console.log('onFinishTrimming', event);
+          handleUpload(event.outputPath)
+          closeEditor();
+          break;
+        }
+        case 'onCancelTrimming': {
+          console.log('onCancelTrimming', event);
+          break;
+        }
+        case 'onCancel': {
+          console.log('onCancel', event);
+          break;
+        }
+        case 'onError': {
+          console.log('onError', event);
+          break;
+        }
+        case 'onLog': {
+          console.log('onLog', event);
+          break;
+        }
+        case 'onStatistics': {
+          console.log('onStatistics', event);
+          break;
+        }
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+  const uri = 'file:///data/user/0/com.gokgokgok/cache/rn_image_picker_lib_temp_75ad52c0-8c9b-47a0-80cc-e3170be56654.mp4'
   return (
     <View style={styles.container}>
       <VideoPlayer 
