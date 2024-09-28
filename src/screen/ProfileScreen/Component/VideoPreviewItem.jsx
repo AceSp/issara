41e7cfc9231579a5101dde95d42c04337a32c49d
AceSp <56382861+AceSp@users.noreply.github.com>
2024-09-28 14:03:46 +0700

    refactor: adjust video preview item dimensions and aspect ratio

--- src/screen/ProfileScreen/Component/VideoPreviewItem.jsx
+++ src/screen/ProfileScreen/Component/VideoPreviewItem.jsx
@@ -3,12 +3,11 @@
 
  const VideoPreviewItem = ({ item, navigation }) => {
      return (
-         <View style={{ flex: 1, margin: 2, width: '30%', height: 120 }}>
+         <View style={{ flex: 1, margin: 2, width: '30%', aspectRatio: 1 }}>
              <Image
                 source={{ uri: item.postInfo.thumbnail }}
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: '100%', height: '100%', aspectRatio: 1 }}
              />
-             {/* <Text style={{ textAlign: 'center', position: 'absolute', bottom: 0, left: 0, right: 0 }}>{item.postInfo.title}</Text> */}
          </View>
      );
  };
